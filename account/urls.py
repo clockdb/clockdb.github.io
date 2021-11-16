37645d13cc569cfdc9a5cb09b5ab19f60cfd24b7
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-16 09:00:00 -0800

    1-2

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from account.views import (
 	account_view,
 	edit_account_view,
-	crop_image,
+	#crop_image,
 )
 
 app_name = 'account'
@@ -11,5 +11,5 @@ app_name = 'account'
 urlpatterns = [
 	path('<user_id>/', account_view, name="view"),
 	path('<user_id>/edit/', edit_account_view, name="edit"),
-	path('<user_id>/edit/cropImage/', crop_image, name="crop_image"),
+	#path('<user_id>/edit/cropImage/', crop_image, name="crop_image"),
 ]
