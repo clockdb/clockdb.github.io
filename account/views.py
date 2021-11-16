c2edf139f251efe5b14799d7f12ee9dbcb64b527
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-16 08:56:08 -0800

    1-2

--- account/views.py
+++ account/views.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.core.files.storage import default_storage
 from django.core.files.storage import FileSystemStorage
 import os
-import cv2
 import json
 import base64
 import requests
@@ -193,46 +192,6 @@ def save_temp_profile_image_from_base64String(imageString, user):
 			imageString += "=" * ((4 - len(imageString) % 4) % 4)
 			return save_temp_profile_image_from_base64String(imageString, user)
 	return None
-
-def crop_image(request, *args, **kwargs):
-	payload = {}
-	user = request.user
-	if request.POST and user.is_authenticated:
-		try:
-			imageString = request.POST.get("image")
-			url = save_temp_profile_image_from_base64String(imageString, user)
-			img = cv2.imread(url)
-
-			cropX = int(float(str(request.POST.get("cropX"))))
-			cropY = int(float(str(request.POST.get("cropY"))))
-			cropWidth = int(float(str(request.POST.get("cropWidth"))))
-			cropHeight = int(float(str(request.POST.get("cropHeight"))))
-			if cropX < 0:
-				cropX = 0
-			if cropY < 0: # There is a bug with cropperjs. y can be negative.
-				cropY = 0
-			crop_img = img[cropY:cropY+cropHeight, cropX:cropX+cropWidth]
-
-			cv2.imwrite(url, crop_img)
-
-			# delete the old image
-			user.profile_image.delete()
-
-			# Save the cropped image to user model
-			user.profile_image.save("profile_image.png", files.File(open(url, 'rb')))
-			user.save()
-
-			payload['result'] = "success"
-			payload['cropped_profile_image'] = user.profile_image.url
-
-			# delete temp file
-			os.remove(url)
-			
-		except Exception as e:
-			print("exception: " + str(e))
-			payload['result'] = "error"
-			payload['exception'] = str(e)
-	return HttpResponse(json.dumps(payload), content_type="application/json")
 
 def edit_account_view(request, *args, **kwargs):
 	if not request.user.is_authenticated:
