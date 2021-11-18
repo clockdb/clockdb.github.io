85ba224594f61e7f1d453ecf3068419659c6413a
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-17 17:03:18 -0800

    apps

--- clock/urls.py
+++ clock/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from workingPaper import views
+from workingPaper.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_screen_view, name="home")
+
+	path('', home_screen_view, name='home'),
 ]
--- workingPaper/views.py
+++ workingPaper/views.py
@@ -1,8 +1,32 @@
-from django.http.response import HttpResponse
-from django.http import HttpResponse
-from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, JsonResponse
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from itertools import chain
+from urllib.parse import urlencode
+
+from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+
+import os
+import cv2
+import json
+import base64
+import requests
+
+from django.core import files
+
+import datetime
+import time
+import json
+
+DEBUG = False
 
 def home_screen_view(request):
-    return HttpResponse('Hello, world!')
+	return render(request, 'templates/home.html')
+
