5d9ca0e3927228739969255190a583f705095952
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-17 16:06:55 -0800

    m

--- clock/urls.py
+++ clock/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from workingPaper.views import *
+from workingPaper import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-	path('', home_screen_view, name='home'),
+    path('', views.home_screen_view, name="home")
 ]
