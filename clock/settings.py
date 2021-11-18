b35a8c8c72d71d1502d5815487d26f940a286fa6
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-17 18:15:53 -0800

    bootstrap

--- clock/settings.py
+++ clock/settings.py
@@ -187,9 +187,9 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = config('AWS_LOCATION')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
