1b5477c8bb36477efa87c7bff23a7a19cdae157d
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-17 17:21:35 -0800

    apps

--- workingPaper/views.py
+++ workingPaper/views.py
@@ -1,8 +1,42 @@
-from django.http.response import HttpResponse
-from django.http import HttpResponse
-from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, JsonResponse
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from itertools import chain
+from urllib.parse import urlencode
+
+from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+
+import os
+import json
+import base64
+import requests
+
+from django.core import files
+
+from account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
+from account.models import *
+
+from friend.utils import get_friend_request_or_false
+from friend.friend_request_status import FriendRequestStatus
+from friend.models import FriendList, FriendRequest
+
+import datetime
+import time
+import json
+
+from chat.models import PrivateChatRoom, RoomChatMessage
+from chat.utils import find_or_create_private_chat
+
+
+
+DEBUG = False
 
 def home_screen_view(request):
     return HttpResponse('Hello, world!')
