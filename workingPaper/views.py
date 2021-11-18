3323960432ed80847f73cd3ca0bd1825db1e9463
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-17 17:16:42 -0800

    apps

--- workingPaper/views.py
+++ workingPaper/views.py
@@ -1,32 +1,8 @@
-from .models import *
+from django.http.response import HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render
 
-from django.contrib.auth import login, authenticate, logout
-from django.conf import settings
-from django.core.files.storage import default_storage
-from django.core.files.storage import FileSystemStorage
-from django.http import HttpResponse, JsonResponse
-from django.urls import reverse
-from django.shortcuts import render, redirect
-from itertools import chain
-from urllib.parse import urlencode
-
-from django.core.files.storage import default_storage
-from django.core.files.storage import FileSystemStorage
-
-import os
-import cv2
-import json
-import base64
-import requests
-
-from django.core import files
-
-import datetime
-import time
-import json
-
-DEBUG = False
+# Create your views here.
 
 def home_screen_view(request):
-	return render(request, 'templates/home.html')
-
+    return HttpResponse('Hello, world!')
