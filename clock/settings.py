ff0fb4c98ec49748e32f39b318edf51cf8f4c8a8
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-17 18:13:30 -0800

    bootstrap

--- clock/settings.py
+++ clock/settings.py
@@ -191,7 +191,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 TEMP = os.path.join(BASE_DIR, 'temp')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
