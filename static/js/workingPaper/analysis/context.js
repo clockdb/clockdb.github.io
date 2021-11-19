bb7fdd73289177a993ece9cdc91f2037aa694d10
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-19 14:47:25 -0800

    js

--- static/js/workingPaper/analysis/context.js
+++
@@ -1,7 +0,0 @@
-
-// analysis/context.js
-console.log('analysis/context.js')
-
-function Context() {
-    document.getElementById('EntityRegistrantNameContext').innerHTML = document.getElementById('EntityRegistrantName1').value;
-}
