7ad60d76fa2f91b5d3cb3ba5c941a3ce38069edf
clockdb <74428678+clockdb@users.noreply.github.com>
2021-11-20 08:29:05 -0800

    js

--- static/js/dom/dom.js
+++ static/js/dom/dom.js
@@ -334,25 +334,3 @@ document.addEventListener('keydown', (event) => {
         }
     }
 });
-
-// tabulation key
-document.addEventListener('keydown', (event) => {
-    keys = [9]
-    if (keys.includes(event.keyCode)) {
-        a = event.target.id
-        //
-        if (a == 'SearchBar') {
-            event.preventDefault();
-            b = document.getElementById('SearchUsersBar')
-            b.select()
-            document.getElementsByClassName('HeaderMenu')[0].scrollTo(9999, 0)
-        }
-        if (a == 'SearchUsersBar') {
-            event.preventDefault();
-            b = document.getElementById('SearchBar')
-            b.select()
-            document.getElementsByClassName('HeaderMenu')[0].scrollTo(0, 9999)
-        }
-    }
-})
-
